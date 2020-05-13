950f056b0b98eac68bd8b7144d5281dd97a067f6
Sweta8826 <shyshweta06@gmail.com>
2020-05-13 18:45:03 +0530

    New chnages

--- src/cloningExample/Test.java
+++ src/cloningExample/Test.java
@@ -4,10 +4,11 @@ public class Test implements Cloneable {
 
 	int x;
 	int z;
-
-	Test(int x,int z){
+	int y;
+	Test(int x,int z,int y){
 		this.x=x;
 		this.z=z;
+		this.y=y;
 	}
 
 	Test Clone(){
@@ -25,17 +26,18 @@ public class Test implements Cloneable {
 
 	public static void main(String args[]) throws Exception {
 
-		Test t1=new Test(1,2);
+		Test t1=new Test(1,2,3);
 
 
 		Test t2= t1.Clone();
 		t1.x=3;
 		t1.z=4;
+		t1.y=6;
 		System.out.println("Object is cloned");
-		System.out.println("Return is:"+t1.x+"Return of y is:"+t1.z);
-		System.out.println("Return of x is:"+t2.x+"Return of y is:"+t2.z);
+		System.out.println("Return is:"+t1.x+"Return of z is:"+t1.z+"Return of y is :"+t1.y);
+		System.out.println("Return of x is:"+t2.x+"Return of z is:"+t2.z+"Return of y is:"+t2.y);
 		
-		Object tfrv =new Test(1,2);
+		Object tfrv =new Test(1,2,3);
 		Test rcrr = (Test) tfrv;
 	}
 
